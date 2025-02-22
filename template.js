0f18b7e0da02adcd6917434fbfd84a2fc574fbb2
Xemt <92655476+Xemt@users.noreply.github.com>
2025-02-22 00:06:43 -0500

    Create template.js

---
+++ template.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Template
+// @version      1.0.0
+// @description  2/22/25.
+// @author       Xemt
+// @copyright    2025
+// @license      MIT
+// @grant        none
+// @run-at       document-idle
+// @match        http://*
+// @match        https://*
+// @icon         none
+// ==/UserScript==
+
+"use strict";
